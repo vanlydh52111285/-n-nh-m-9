520ac906ef8074e7cbf9639e972cd8e8c3718dba
vanlydh52111285 <dh52111285@student.stu.edu.vn>
2023-12-17 16:20:42 +0700

    Dat Lich

---
+++ nhom_9/DanhSachDatLich.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Drawing;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+namespace nhom_9
+{
+    [Serializable]
+    internal class DanhSachDatLich
+    {
+        private List<DatLich> _danhSachDatLich;
+        
+        public DanhSachDatLich()
+        {
+            _danhSachDatLich = new List<DatLich>();
+        }
+        public List<DatLich> danhSachDatLich
+        {
+            get { return this._danhSachDatLich.ToList(); }
+            set { this._danhSachDatLich = value; }
+        }
+        public void AddDanhSachDatLich(DatLich dsdt)
+        {
+           
+            this._danhSachDatLich.Add(dsdt);
+        }
+      
+        public bool timTG(DatLich dl)
+        {
+            foreach (DatLich d in this._danhSachDatLich)
+            {
+                if (d.thoigian.Equals(dl.thoigian))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public DatLich TimTheoID(string id)
+        {
+            return _danhSachDatLich.FirstOrDefault(d => d.id == id);
+        }
+        public void SuaDanhSachDatLich(string id, DatLich datLichMoi)
+        {
+
+            DatLich datLichCanSua = _danhSachDatLich.FirstOrDefault(d => d.id == id);
+
+            if (datLichCanSua != null)
+            {
+
+                datLichCanSua.tieude = datLichMoi.tieude;
+                datLichCanSua.noidung = datLichMoi.noidung;
+                datLichCanSua.thoigian = datLichMoi.thoigian;
+                datLichCanSua.trangthai = datLichMoi.trangthai;
+
+
+
+            }
+
+        }
+
+
+        public void XoaDanhSachDatLich(string id)
+        {
+            
+            DatLich datLichCanXoa = _danhSachDatLich.FirstOrDefault(d => d.id == id);
+
+            if (datLichCanXoa != null)
+            {
+            
+                _danhSachDatLich.Remove(datLichCanXoa);
+               
+            }
+        }
+      
+      
+        
+
+    }
+}
+
+
---
+++ nhom_9/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace nhom_9
+{
+    partial class frm_dat_lich
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.frm_thoatchuongtrinh = new System.Windows.Forms.Button();
+            this.frm_dang_xuat = new System.Windows.Forms.Button();
+            this.btn_tao_moi = new System.Windows.Forms.Button();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.chk_hoanthanh = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dtp_thoigian = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_luu_lich = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_noidung = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_tieude = new System.Windows.Forms.TextBox();
+            this.btn_timkiem = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_timkiem = new System.Windows.Forms.TextBox();
+            this.dgv_danhsach = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_danhsach)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.Cyan;
+            this.splitContainer1.Panel1.Controls.Add(this.frm_thoatchuongtrinh);
+            this.splitContainer1.Panel1.Controls.Add(this.frm_dang_xuat);
+            this.splitContainer1.Panel1.Controls.Add(this.btn_tao_moi);
+            this.splitContainer1.Panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.splitContainer1.Size = new System.Drawing.Size(1099, 513);
+            this.splitContainer1.SplitterDistance = 225;
+            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // frm_thoatchuongtrinh
+            // 
+            this.frm_thoatchuongtrinh.AllowDrop = true;
+            this.frm_thoatchuongtrinh.Location = new System.Drawing.Point(53, 479);
+            this.frm_thoatchuongtrinh.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.frm_thoatchuongtrinh.Name = "frm_thoatchuongtrinh";
+            this.frm_thoatchuongtrinh.Size = new System.Drawing.Size(86, 31);
+            this.frm_thoatchuongtrinh.TabIndex = 2;
+            this.frm_thoatchuongtrinh.Text = "Thoát";
+            this.frm_thoatchuongtrinh.UseCompatibleTextRendering = true;
+            this.frm_thoatchuongtrinh.UseVisualStyleBackColor = true;
+            // 
+            // frm_dang_xuat
+            // 
+            this.frm_dang_xuat.AllowDrop = true;
+            this.frm_dang_xuat.Location = new System.Drawing.Point(53, 441);
+            this.frm_dang_xuat.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.frm_dang_xuat.Name = "frm_dang_xuat";
+            this.frm_dang_xuat.Size = new System.Drawing.Size(86, 31);
+            this.frm_dang_xuat.TabIndex = 1;
+            this.frm_dang_xuat.Text = "Đăng Xuất";
+            this.frm_dang_xuat.UseCompatibleTextRendering = true;
+            this.frm_dang_xuat.UseVisualStyleBackColor = true;
+            // 
+            // btn_tao_moi
+            // 
+            this.btn_tao_moi.Location = new System.Drawing.Point(53, 19);
+            this.btn_tao_moi.Name = "btn_tao_moi";
+            this.btn_tao_moi.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btn_tao_moi.Size = new System.Drawing.Size(94, 29);
+            this.btn_tao_moi.TabIndex = 0;
+            this.btn_tao_moi.Text = "Tạo mới";
+            this.btn_tao_moi.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.splitContainer2.Panel1.Controls.Add(this.chk_hoanthanh);
+            this.splitContainer2.Panel1.Controls.Add(this.button2);
+            this.splitContainer2.Panel1.Controls.Add(this.button1);
+            this.splitContainer2.Panel1.Controls.Add(this.dtp_thoigian);
+            this.splitContainer2.Panel1.Controls.Add(this.label2);
+            this.splitContainer2.Panel1.Controls.Add(this.btn_luu_lich);
+            this.splitContainer2.Panel1.Controls.Add(this.label4);
+            this.splitContainer2.Panel1.Controls.Add(this.txt_noidung);
+            this.splitContainer2.Panel1.Controls.Add(this.label1);
+            this.splitContainer2.Panel1.Controls.Add(this.txt_tieude);
+            this.splitContainer2.Panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.splitContainer2.Panel2.Controls.Add(this.btn_timkiem);
+            this.splitContainer2.Panel2.Controls.Add(this.label3);
+            this.splitContainer2.Panel2.Controls.Add(this.txt_timkiem);
+            this.splitContainer2.Panel2.Controls.Add(this.dgv_danhsach);
+            this.splitContainer2.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.splitContainer2.Size = new System.Drawing.Size(869, 513);
+            this.splitContainer2.SplitterDistance = 254;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // chk_hoanthanh
+            // 
+            this.chk_hoanthanh.AutoSize = true;
+            this.chk_hoanthanh.Location = new System.Drawing.Point(581, 17);
+            this.chk_hoanthanh.Name = "chk_hoanthanh";
+            this.chk_hoanthanh.Size = new System.Drawing.Size(108, 24);
+            this.chk_hoanthanh.TabIndex = 13;
+            this.chk_hoanthanh.Text = "Hoàn thành";
+            this.chk_hoanthanh.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(581, 161);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Cập nhật";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(581, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Xóa";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dtp_thoigian
+            // 
+            this.dtp_thoigian.CustomFormat = "      HH:mm                     dd/MM/yyyy";
+            this.dtp_thoigian.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtp_thoigian.Location = new System.Drawing.Point(112, 212);
+            this.dtp_thoigian.Name = "dtp_thoigian";
+            this.dtp_thoigian.Size = new System.Drawing.Size(450, 27);
+            this.dtp_thoigian.TabIndex = 10;
+            this.dtp_thoigian.Value = new System.DateTime(2023, 11, 12, 20, 13, 0, 0);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Thời gian:";
+            // 
+            // btn_luu_lich
+            // 
+            this.btn_luu_lich.Location = new System.Drawing.Point(581, 211);
+            this.btn_luu_lich.Name = "btn_luu_lich";
+            this.btn_luu_lich.Size = new System.Drawing.Size(94, 29);
+            this.btn_luu_lich.TabIndex = 6;
+            this.btn_luu_lich.Text = "Lưu lịch";
+            this.btn_luu_lich.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nội dung :";
+            // 
+            // txt_noidung
+            // 
+            this.txt_noidung.Location = new System.Drawing.Point(112, 55);
+            this.txt_noidung.Multiline = true;
+            this.txt_noidung.Name = "txt_noidung";
+            this.txt_noidung.Size = new System.Drawing.Size(450, 135);
+            this.txt_noidung.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tiêu đề :";
+            // 
+            // txt_tieude
+            // 
+            this.txt_tieude.Location = new System.Drawing.Point(112, 12);
+            this.txt_tieude.Name = "txt_tieude";
+            this.txt_tieude.Size = new System.Drawing.Size(450, 27);
+            this.txt_tieude.TabIndex = 0;
+            // 
+            // btn_timkiem
+            // 
+            this.btn_timkiem.Location = new System.Drawing.Point(581, 19);
+            this.btn_timkiem.Name = "btn_timkiem";
+            this.btn_timkiem.Size = new System.Drawing.Size(94, 29);
+            this.btn_timkiem.TabIndex = 3;
+            this.btn_timkiem.Text = "SREACH";
+            this.btn_timkiem.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tìm kiếm";
+            // 
+            // txt_timkiem
+            // 
+            this.txt_timkiem.Location = new System.Drawing.Point(87, 19);
+            this.txt_timkiem.Name = "txt_timkiem";
+            this.txt_timkiem.Size = new System.Drawing.Size(475, 27);
+            this.txt_timkiem.TabIndex = 1;
+            // 
+            // dgv_danhsach
+            // 
+            this.dgv_danhsach.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.dgv_danhsach.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_danhsach.GridColor = System.Drawing.SystemColors.ActiveBorder;
+            this.dgv_danhsach.Location = new System.Drawing.Point(11, 51);
+            this.dgv_danhsach.Name = "dgv_danhsach";
+            this.dgv_danhsach.RowHeadersWidth = 51;
+            this.dgv_danhsach.Size = new System.Drawing.Size(843, 196);
+            this.dgv_danhsach.TabIndex = 0;
+            // 
+            // frm_dat_lich
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1099, 513);
+            this.Controls.Add(this.splitContainer1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frm_dat_lich";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đặt lịch";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_danhsach)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private SplitContainer splitContainer2;
+        private TextBox txt_noidung;
+        private Label label1;
+        private Button btn_luu_lich;
+        private Label label4;
+        private TextBox txt_tieude;
+        private DataGridView dgv_danhsach;
+        private Label label2;
+        private Button btn_tao_moi;
+        private Button btn_timkiem;
+        private Label label3;
+        private TextBox txt_timkiem;
+        private DateTimePicker dtp_thoigian;
+        private Button button2;
+        private Button button1;
+        private CheckBox chk_hoanthanh;
+        private Button frm_dang_xuat;
+        private Button frm_thoatchuongtrinh;
+    }
+}
