c2027aaf6b99d630059eff9fccb54a914e1b5e85
vanlydh52111285 <dh52111285@student.stu.edu.vn>
2023-12-18 16:16:58 +0700

    thongbao

--- nhom_9/thongbao.cs
+++ nhom_9/thongbao.cs
@@ -10,7 +10,7 @@ namespace nhom_9
     class ThongBao
     {
         private System.Timers.Timer _timer;
-        public void setTask(DateTime dueTime)
+        public void setTask(DateTime dueTime, string tieude)
         {
             TimeSpan alertSpan = TimeSpan.FromMinutes(3); // Thời gian cảnh báo trước 3 phút
             DateTime alertTime = dueTime.Subtract(alertSpan);
@@ -21,7 +21,7 @@ namespace nhom_9
             {
                 // Tạo timer để thông báo trước 3 phút
                 System.Timers.Timer alertTimer = new System.Timers.Timer(alertInterval);
-                alertTimer.Elapsed += AlertEvent;
+                alertTimer.Elapsed += (sender, e) =>AlertEvent(sender, e, tieude);
                 alertTimer.AutoReset = false;
                 alertTimer.Enabled = true;
             }
@@ -30,7 +30,7 @@ namespace nhom_9
             {
                 // Tạo timer để thông báo đến thời gian làm việc
                 _timer = new System.Timers.Timer(taskInterval);
-                _timer.Elapsed += OnTimedEvent;
+                _timer.Elapsed += (sender, e) =>OnTimedEvent(sender, e, tieude);
                 _timer.AutoReset = false;
                 _timer.Enabled = true;
             }
@@ -39,13 +39,13 @@ namespace nhom_9
                 MessageBox.Show("Thông báo sắp đến giờ", "Nhắc Nhở");
             }
         }
-        private void AlertEvent(object source, ElapsedEventArgs e)
+        private void AlertEvent(object source, ElapsedEventArgs e, string tieude)
         {
-            MessageBox.Show("Sắp đến giờ làm việc", "Nhắc Nhở");
+            MessageBox.Show("Sắp đến giờ làm việc "+tieude, "Nhắc Nhở");
         }
-        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        private void OnTimedEvent(Object source, ElapsedEventArgs e, string tieude)
         {
-            MessageBox.Show("Đến thời gian làm việc", "Nhắc nhở");
+            MessageBox.Show("Đến thời gian làm việc "+tieude, "Nhắc nhở");
             _timer.Stop();
             _timer.Dispose();
         }
