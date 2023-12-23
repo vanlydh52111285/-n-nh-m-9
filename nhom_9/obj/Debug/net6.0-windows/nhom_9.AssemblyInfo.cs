5d23ca743c4679db3e024183fdd513ff67e715bf
vuducminh2112 <dh52111325@student.stu.edu.vn>
2023-12-23 21:07:56 +0700

    update

--- nhom_9/obj/Debug/net6.0-windows/nhom_9.AssemblyInfo.cs
+++ nhom_9/obj/Debug/net6.0-windows/nhom_9.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("nhom_9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db460978d6e8d2fd64be66012f57bbbd20342177")]
 [assembly: System.Reflection.AssemblyProductAttribute("nhom_9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("nhom_9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
